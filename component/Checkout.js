0a5546b1696d4b3fbdc567cf071bbdee8c57ae23
HN721 <hosea1422@gmail.com>
2025-04-26 18:45:45 +0700

    feat: add function checkout to create user checkout

--- component/Checkout.js
+++ component/Checkout.js
@@ -1,5 +1,21 @@
 import { input } from "@inquirer/prompts";
+import keranjang from "./Cartdata.js";
 
-async function checkout() {
-  const answer = await input({ message: `Apakah Kamu Yakin? (Y/N):` });
+export default async function checkout() {
+  const nama = await input({ message: `Masukan Nama Anda` });
+  const email = await input({ message: `Masukan Email Anda` });
+  console.log("Pesanan Anda:");
+  keranjang.forEach((item, index) =>
+    console.log(`${index + 1}. ${item.nama} dengan harga: ${item.harga}`)
+  );
+
+  const pay = await input({ message: `Apakah Sesuai ? Y/N` });
+  if (pay === "y") {
+    console.log(`----------Invoice---------- `);
+    console.log(`Nama: ${nama}`);
+    console.log(`Email: ${email}`);
+    keranjang.forEach((item, index) =>
+      console.log(`Pesanan kamu: ${item.nama} Rp.${item.harga}`)
+    );
+  }
 }
